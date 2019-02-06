605c2663a84bf079e8265e8af7b762af30a79ae3
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-02-06 13:50:42 -0300

    Update index.php

--- public/adminux/index.php
+++ public/adminux/index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Admin 3</title>
+<title>Admin 4</title>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha256-azvvU9xKluwHFJ0Cpgtf0CYzK7zgtOznnzxV4924X1w=" crossorigin="anonymous" rel="stylesheet" />
 <link href="resources/admin.css" rel="stylesheet">
 </head>
