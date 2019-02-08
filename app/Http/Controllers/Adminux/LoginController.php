aa1518ff9fb407436f30a84d0c9e6b39c31a85a2
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-02-08 18:32:39 -0300

    login

--- app/Http/Controllers/Adminux/LoginController.php
+++ app/Http/Controllers/Adminux/LoginController.php
@@ -4,11 +4,35 @@ namespace App\Http\Controllers\Adminux;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 
 class LoginController extends Controller
 {
-    public function login()
+    use AuthenticatesUsers;
+
+    protected function guard()
+    {
+        return Auth::guard('adminux');
+    }
+
+    public function username()
+    {
+        return 'username';
+    }
+
+    public function showLoginForm()
     {
         return view('adminux/login');
     }
+
+    // protected function guard()
+    // {
+    //     return Auth::guard('adminux');
+    // }
+    //
+    // public function login()
+    // {
+    //     return view('adminux/login');
+    // }
 }
--- resources/views/adminux/login.blade.php
+++ resources/views/adminux/login.blade.php
@@ -14,20 +14,17 @@
                 <div class="row justify-content-center">
                     <div class="col-md-8">
                         <div class="card">
-                            <div class="card-header">{{ config('app.name', 'Admin') }} - {{ __('Login') }}</div>
+                            <div class="card-header">{{ config('app.name', 'Admin') }}</div>
                             <div class="card-body">
                                 <form method="POST" action="{{ route('login') }}">
                                     @csrf
-
                                     <div class="form-group row">
-                                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
-
+                                        <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
                                         <div class="col-md-6">
-                                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                            @if ($errors->has('email'))
+                                            <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" required autofocus>
+                                            @if ($errors->has('username'))
                                                 <span class="invalid-feedback" role="alert">
-                                                    <strong>{{ $errors->first('email') }}</strong>
+                                                    <strong>{{ $errors->first('username') }}</strong>
                                                 </span>
                                             @endif
                                         </div>
@@ -35,10 +32,8 @@
 
                                     <div class="form-group row">
                                         <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
                                         <div class="col-md-6">
                                             <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
                                             @if ($errors->has('password'))
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $errors->first('password') }}</strong>
@@ -64,7 +59,6 @@
                                             <button type="submit" class="btn btn-primary">
                                                 {{ __('Login') }}
                                             </button>
-
                                             @if (Route::has('password.request'))
                                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                                     {{ __('Forgot Your Password?') }}
