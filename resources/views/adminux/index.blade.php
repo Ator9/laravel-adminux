136d9210ead40fdc4ca1e667cf6b277288c56ae2
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-02-09 17:38:28 -0300

    login

--- resources/views/adminux/index.blade.php
+++ resources/views/adminux/index.blade.php
@@ -11,6 +11,7 @@
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ asset('admin') }}">{{ config('app.name', 'Admin') }}</a>
     <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    <span>{{ Auth::guard('adminux')->user()->email }}</span>
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
             <a class="nav-link" href="{{ asset('admin/logout') }}">Sign out</a>
