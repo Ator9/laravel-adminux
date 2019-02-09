ef848c9c5ef3a9b62d9c421a2b990139a20585c2
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-02-09 19:33:12 -0300

    login

--- resources/views/adminux/index.blade.php
+++ resources/views/adminux/index.blade.php
@@ -1,24 +1,8 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>{{ config('app.name', 'Admin') }}</title>
-<link href="{{ asset('adminux/resources//libs/bootstrap.min.css') }}" rel="stylesheet" />
-<link href="{{ asset('adminux/resources/admin.css') }}" rel="stylesheet">
-</head>
-<body>
-<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ asset('admin') }}">{{ config('app.name', 'Admin') }}</a>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-    <span class="text-white px-3">{{ Auth::guard('adminux')->user()->email }}</span>
-    <ul class="navbar-nav mr-3">
-        <li class="nav-item">
-            <a class="nav-link" href="{{ asset('admin/logout') }}"><span data-feather="log-out"></span></a>
-        </li>
-    </ul>
-</nav>
+@extends('adminux.layout')
 
+@section('title', config('app.name', 'Admin'))
+
+@section('body')
 <div class="container-fluid">
     <div class="row">
         <nav class="col-md-2 d-none d-md-block bg-light sidebar">
@@ -245,10 +229,4 @@
         </main>
     </div>
 </div>
-<script src="{{ asset('adminux/resources//libs/jquery.min.js') }}"></script>
-<script src="{{ asset('adminux/resources//libs/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('adminux/resources//libs/feather.min.js') }}"></script>
-<script src="{{ asset('adminux/resources//libs/Chart.min.js') }}"></script>
-<script src="{{ asset('adminux/resources/admin.js') }}"></script>
-</body>
-</html>
+@endsection
--- resources/views/adminux/login.blade.php
+++ resources/views/adminux/login.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('adminux.layout')
+
+@section('title', config('app.name', 'Admin'))
+
+@section('head')
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<title>{{ config('app.name', 'Admin') }}</title>
 <link href="{{ asset('adminux/resources/login.css') }}" rel="stylesheet">
-</head>
-<body>
+@endsection
+
+@section('body')
 <div id="app">
     <main class="py-4">
         <div class="container">
@@ -57,7 +57,4 @@
         </div>
     </main>
 </div>
-<script src="{{ asset('adminux/resources//libs/jquery.min.js') }}"></script>
-<script src="{{ asset('adminux/resources//libs/bootstrap.bundle.min.js') }}"></script>
-</body>
-</html>
+@endsection
