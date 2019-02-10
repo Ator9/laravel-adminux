ac7e64642b12fa9a01097594306d1d12e18ab51b
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-02-10 16:42:16 -0300

    login

--- resources/views/adminux/admins/index.blade.php
+++ resources/views/adminux/admins/index.blade.php
@@ -6,10 +6,9 @@
 <div class="container-fluid">
     <div class="row">
         @include('adminux.layout.navleft')
-
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
-            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 class="h2">Dashboard</h1>
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2">
+                <h1 class="h2">Admins</h1>
                 <div class="btn-toolbar mb-2 mb-md-0">
                     <div class="btn-group mr-2">
                         <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
@@ -22,9 +21,6 @@
                 </div>
             </div>
 
-            <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
-
-            <h2>Section title</h2>
             <div class="table-responsive">
                 <table class="table table-striped table-sm">
                     <thead>
