222d73f549cb966e15a123fec4a4b4b66503dd0c
Sebastian Gasparri <sgasparri@adverit.com>
2019-02-11 15:49:07 -0300

    admin

--- resources/views/adminux/layout/navtop.blade.php
+++ resources/views/adminux/layout/navtop.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ asset('admin') }}">{{ config('app.name', 'Admin') }}</a>
-    <ul class="nav w-100 ml-2">
+    <ul class="nav w-100 ml-2 h6">
         <li class="nav-item">
             <a class="nav-link text-white disabled" href="#">Admins</a>
         </li>
