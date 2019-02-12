35e61083e8f943fe3eccb9d1ebccfa33f91b2662
Sebastian Gasparri <sgasparri@adverit.com>
2019-02-12 15:21:30 -0300

    admin

---
+++ app/Adminux/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+Route::namespace('\App\Adminux')->group(function() {
+    Route::post('login', 'LoginController@login')->name('login');
+    Route::get('login', 'LoginController@showLoginForm')->name('showLoginForm');
+    Route::get('logout', 'LoginController@logout')->name('logout');
+    Route::get('', 'Admins\Controllers\AdminController@dashboard')->name('admin.dashboard');
+
+    Route::resource('admins', 'Admins\Controllers\AdminController');
+    Route::resource('partners', 'Partners\Controllers\PartnerController');
+});
+
+if(file_exists(__DIR__.'/routescustom.php')) include __DIR__.'/routescustom.php';
