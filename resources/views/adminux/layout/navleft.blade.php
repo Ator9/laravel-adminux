a58e181011965c2553537de7076af1f7fe1c1762
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-02-13 19:13:01 -0300

    helpers

--- resources/views/adminux/layout/navleft.blade.php
+++ resources/views/adminux/layout/navleft.blade.php
@@ -1,20 +1,20 @@
 <nav class="col-md-2 d-none d-md-block bg-light sidebar">
     <div class="sidebar-sticky">
         <ul class="nav flex-column">
-            @inject('Helpers', 'App\Adminux\Helpers')
             <li class="nav-item">
                 <a class="nav-link{{ Request::is('admin') ? ' active' : '' }}" href="{{ asset('admin') }}">
                     <span data-feather="home"></span>
                     Dashboard
                 </a>
             </li>
+            @inject('Helpers', 'App\Adminux\Helpers')
             @foreach($Helpers->getNavLeft() as $module)
-            <li class="nav-item">
-                <a class="nav-link{{ Request::is('admin/'.strtolower($module['dir'])) ? ' active' : '' }}" href="{{ asset('admin/'.strtolower($module['dir'])) }}">
-                    <span data-feather="{{ $module['icon'] }}"></span>
-                    {{ $module['name'] }}
-                </a>
-            </li>
+                <li class="nav-item">
+                    <a class="nav-link{{ Request::is('admin/'.strtolower($module['dir'])) ? ' active' : '' }}" href="{{ asset('admin/'.strtolower($module['dir'])) }}">
+                        <span data-feather="{{ $module['icon'] }}"></span>
+                        {{ $module['name'] }}
+                    </a>
+                </li>
             @endforeach
         </ul>
 
