396ca31f68fbc2eab51d190882fcfd9c8272cf44
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-02-23 10:36:00 -0300

    fix

--- resources/views/adminux/components/layout/navtop.blade.php
+++ resources/views/adminux/components/layout/navtop.blade.php
@@ -1,8 +1,12 @@
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="{{ asset('admin') }}">{{ config('app.name', 'Admin') }}</a>
     <ul class="nav w-100">
+        <?php
+        $explode = explode('_', request()->segment(2));
+        $module = reset($explode);
+        ?>
         @foreach($Helpers->getNavTop(Request::path()) as $dir => $name)
-            @if ($loop->index > 0 and $dir = 'admin_'.$dir) @endif
+            @if ($loop->index > 0 and $dir = $module.'_'.$dir) @endif
             <li class="nav-item">
                 @if(Request::is('admin/'.$dir) or $loop->count == 1 or (isset($model)))
                     <a class="nav-link text-white" href="{{ asset('admin/'.$dir) }}">{{ $name }}</a>
