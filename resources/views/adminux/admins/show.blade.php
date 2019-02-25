40cb38fb51d9a1a29a7d348882a04c0c1ed6c745
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-02-25 18:34:05 -0300

    nav

--- resources/views/adminux/admins/show.blade.php
+++ resources/views/adminux/admins/show.blade.php
@@ -3,7 +3,13 @@
 @section('title', 'Dashboard' . ' - ' . config('app.name', 'Admin'))
 
 @section('body')
-<div class="mt-2">
-    <small><a href="">Brief description</a> > Product name</small>
+<nav class="mt-3" aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Data</li>
+  </ol>
+</nav>
+<div class="mt-3">
+    <a href="">Brief description</a> > Product name
 </div>
 @endsection
