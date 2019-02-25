0175ccea0db18057ca1a582013051da04e05b199
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-02-25 19:47:54 -0300

    nav

--- resources/views/adminux/admins/show.blade.php
+++ resources/views/adminux/admins/show.blade.php
@@ -3,13 +3,17 @@
 @section('title', 'Dashboard' . ' - ' . config('app.name', 'Admin'))
 
 @section('body')
-<nav class="mt-3" aria-label="breadcrumb">
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="#">Home</a></li>
-    <li class="breadcrumb-item active" aria-current="page">Data</li>
-  </ol>
-</nav>
-<div class="mt-3">
-    <a href="">Brief description</a> > Product name
+<div class="card mt-3">
+  <div class="card-header">
+      <h5 class="d-flex justify-content-between mb-0">
+          <span>Admin Details</span>
+          <a href="{{ Request::url() }}/edit"><span class="feather-adminux" data-feather="edit"></span></a>
+      </h5>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
 </div>
 @endsection
