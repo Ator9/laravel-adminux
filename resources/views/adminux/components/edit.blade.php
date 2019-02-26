64fcb1f46e561c8b3af6a74fb8b03b37faf855a8
Sebastian Gasparri <sgasparri@adverit.com>
2019-02-26 14:44:31 -0300

    edit

--- resources/views/adminux/components/edit.blade.php
+++ resources/views/adminux/components/edit.blade.php
@@ -3,20 +3,22 @@
 @section('title', 'Dashboard' . ' - ' . config('app.name', 'Admin'))
 
 @section('body')
-<div class="card mt-3">
-  <div class="card-header">
-      <h5 class="d-flex justify-content-between mb-0">
-          <span>{{(new ReflectionClass($model))->getShortName()}} Edit</span>
-          <a href="{{ Request::url() }}/edit"><span class="feather-adminux" data-feather="edit"></span></a>
-      </h5>
-  </div>
-  <div class="card-body">
-    @foreach($model->toArray() as $key => $val)
-        <div class="row mb-3">
-            <div class="col-md-2 text-right">{{$key}}</div>
-            <div class="col-md-10 ">{{ $val }}</div>
+<form method="post" action="{{ str_replace('/edit', '', Request::url()) }}" id="form_edit">
+    <div class="card mt-3">
+        <div class="card-header">
+            <h5 class="d-flex justify-content-between mb-0">
+                <span>{{(new ReflectionClass($model))->getShortName()}} Edit</span>
+                <a href="#" onclick="$('#form_edit').submit();return false;"><span class="feather-adminux" data-feather="save"></span></a>
+            </h5>
         </div>
-    @endforeach
-  </div>
-</div>
+        <div class="card-body">
+            @foreach($model->toArray() as $key => $val)
+            <div class="row mb-3">
+                <div class="col-md-2 text-right">{{$key}}</div>
+                <div class="col-md-10 ">{{ $val }}</div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</form>
 @endsection
