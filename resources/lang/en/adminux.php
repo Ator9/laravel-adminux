cd97bcd5faad28a6426d1683b6485d47be9380c6
Sebastian Gasparri <sgasparri@adverit.com>
2019-02-28 11:38:09 -0300

    lang

--- resources/lang/en/adminux.php
+++ resources/lang/en/adminux.php
@@ -2,11 +2,17 @@
 
 return [
 
+    'active' => 'Activo',
+    'created_at' => 'Created At',
+    'deleted_at' => 'Deleted At',
     'details' => 'Details',
     'edit' => 'Edit',
     'email' => 'E-mail',
-    'firstname' => 'First name',
+    'firstname' => 'First Name',
     'id' => 'ID',
-    'lastname' => 'Last name',
-
+    'lastname' => 'Last Name',
+    'last_login_at' => 'Last Login At',
+    'name' => 'Name',
+    'superuser' => 'Superuser',
+    'updated_at' => 'Updated At',
 ];
--- resources/lang/es/adminux.php
+++ resources/lang/es/adminux.php
@@ -2,10 +2,17 @@
 
 return [
 
+    'active' => 'Activo',
+    'created_at' => 'Creado el',
+    'deleted_at' => 'Elminado el',
     'details' => 'Detalles',
     'edit' => 'Editar',
     'email' => 'E-mail',
     'firstname' => 'Nombre',
     'id' => 'ID',
     'lastname' => 'Apellido',
+    'last_login_at' => 'Último Logueo el',
+    'name' => 'Nombre',
+    'superuser' => 'Super Usuario',
+    'updated_at' => 'Actualizado el',
 ];
