4bfee56c90f191a77af24e220ad917b80f6bfcf7
Sebastian Gasparri <sgasparri@adverit.com>
2019-03-07 16:03:23 -0300

    admins partner

---
+++ app/Adminux/Admin/Models/AdminPartner.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Adminux\Admin\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AdminPartner extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'adminux_admins_partners';
+}
