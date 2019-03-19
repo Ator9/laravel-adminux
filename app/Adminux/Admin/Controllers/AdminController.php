561a96bfe5cc18c362ae8f9415868d9fbefb795c
Sebastian Gasparri <sgasparri@adverit.com>
2019-03-19 09:01:45 -0300

    admin form email

--- app/Adminux/Admin/Controllers/AdminController.php
+++ app/Adminux/Admin/Controllers/AdminController.php
@@ -95,7 +95,7 @@ class AdminController extends Controller
     public function update(Request $request, Admin $admin)
     {
         $request->validate([
-            'email'  => 'required',
+            'email'  => 'required|email',
             'active' => 'in:Y',
         ]);
 
