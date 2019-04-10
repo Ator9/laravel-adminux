48dd5b2f222d9869de7924f78bcbadca7b920a5b
Sebastian Gasparri <sgasparri@adverit.com>
2019-04-10 08:36:04 -0300

    title layout

--- resources/views/adminux/components/layout/layout.blade.php
+++ resources/views/adminux/components/layout/layout.blade.php
@@ -3,8 +3,8 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>@yield('title')</title>
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<title>@yield('title')</title>
 <link href="{{ asset('adminux/resources/libs/bootstrap.min.css') }}" rel="stylesheet">
 <link href="{{ asset('adminux/resources/admin.css') }}" rel="stylesheet">
 @yield('head')
