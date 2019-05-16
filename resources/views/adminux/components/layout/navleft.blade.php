2513970fb55955e263a140228f5310631e8a1311
Sebastian Gasparri <sgasparri@adverit.com>
2019-05-16 09:31:29 -0300

    fix css

--- resources/views/adminux/components/layout/navleft.blade.php
+++ resources/views/adminux/components/layout/navleft.blade.php
@@ -3,15 +3,13 @@
         <ul class="nav flex-column">
             <li class="nav-item">
                 <a class="nav-link{{ Request::is('admin') ? ' active' : '' }}" href="{{ asset('admin') }}">
-                    <span data-feather="home"></span>
-                    Dashboard
+                    <span data-feather="home"></span> Dashboard
                 </a>
             </li>
             @foreach($Helper->getNavLeft() as $module)
                 <li class="nav-item">
                     <a class="nav-link{{ (Request::is('admin/'.strtolower($module['dir'])) or str_contains(Request::path(), ['admin/'.strtolower($module['dir']).'_', 'admin/'.strtolower($module['dir']).'/'])) ? ' active' : '' }}" href="{{ asset('admin/'.strtolower($module['dir'])) }}">
-                        <span data-feather="{{ $module['icon'] }}"></span>
-                        {{ $module['name'] }}
+                        <span data-feather="{{ $module['icon'] }}"></span> {{ $module['name'] }}
                     </a>
                 </li>
             @endforeach
