a8ed7aa08a3f8f6bfc2f3a45d3bbdff5c65773d2
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-06-29 09:49:06 -0300

    product update validation

--- app/Adminux/Product/Controllers/ProductController.php
+++ app/Adminux/Product/Controllers/ProductController.php
@@ -99,6 +99,7 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
+        Helper::validatePartner($product);
         $request->validate([ 'product' => 'required' ]);
 
         $product->update($request->only(['product']));
