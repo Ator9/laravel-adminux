828d441398bff86a734788c8376fc8f99562edb4
Sebastian Gasparri <sgasparri@adverit.com>
2019-07-05 08:56:44 -0300

    char

--- app/Adminux/Form.php
+++ app/Adminux/Form.php
@@ -63,7 +63,7 @@ class Form
                 if(isset($params['allows']) && !in_array($val->id, $params['allows'])) continue;
 
                 $sel = ($val->id == $this->getValue($params)) ? ' selected' : '';
-                $options[] = '<option value="'.$val->id.'"'.$sel.'>'.$val->id.' > '.$val->{strtolower($params['label'])}.'</option>';
+                $options[] = '<option value="'.$val->id.'"'.$sel.'>'.$val->id.' - '.$val->{strtolower($params['label'])}.'</option>';
             }
         } else $options = $params['options'];
 
