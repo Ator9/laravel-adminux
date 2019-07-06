d1e6499373b1437d14951506ec4ba22607077cf2
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-07-06 12:14:18 -0300

    lang top

--- resources/views/adminux/components/layout/navtop.blade.php
+++ resources/views/adminux/components/layout/navtop.blade.php
@@ -4,9 +4,9 @@
         @foreach($Helper->getNavTop(Request::path()) as $dir => $name)
             <li class="nav-item">
                 @if(Request::is($prefix.'/'.$dir) or strpos(Request::path(), $prefix.'/'.$dir.'/') !== false)
-                    <a class="nav-link text-white" href="{{ asset($prefix.'/'.$dir) }}">{{ $name }}</a>
+                    <a class="nav-link text-white" href="{{ asset($prefix.'/'.$dir) }}">@if(Lang::has('adminux.'.$name)) {{ __('adminux.'.$name) }} @else {{ $name }} @endif</a>
                 @else
-                    <a class="nav-link text-warning" href="{{ asset($prefix.'/'.$dir) }}">{{ $name }}</a>
+                    <a class="nav-link text-warning" href="{{ asset($prefix.'/'.$dir) }}">@if(Lang::has('adminux.'.$name)) {{ __('adminux.'.$name) }} @else {{ $name }} @endif</a>
                 @endif
             </li>
         @endforeach
