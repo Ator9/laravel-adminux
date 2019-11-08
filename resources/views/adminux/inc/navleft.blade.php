55f8d646d520a4919f0d4095a9e80360e61e3b35
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-11-08 14:50:43 -0300

    fix

--- resources/views/adminux/inc/navleft.blade.php
+++ resources/views/adminux/inc/navleft.blade.php
@@ -6,41 +6,28 @@
                     <span data-feather="home"></span> Dashboard
                 </a>
             </li>
-            @php $menu = $Helper->getNavLeft(); @endphp
-            @foreach($menu as $module)
-                @if(in_array($module['dir'], ['Admins', 'Partners', 'Services'])) @continue; @endif
+            @php $menu = config('adminux.base.default.menu'); @endphp
+            @foreach($menu['default'] as $name => $module)
                 <li class="nav-item">
-                    <a class="nav-link{{ (Request::is($prefix.'/'.strtolower($module['dir'])) or Illuminate\Support\Str::contains(Request::path(), [$prefix.'/'.strtolower($module['dir']).'_', $prefix.'/'.strtolower($module['dir']).'/'])) ? ' active' : '' }}" href="{{ asset($prefix.'/'.strtolower($module['dir'])) }}">
-                        <span data-feather="{{ $module['icon'] }}"></span> {{ $module['name'] }}
-                    </a>
-                    <!-- @if(Request::is($prefix.'/'.strtolower($module['dir'])) or Illuminate\Support\Str::contains(Request::path(), [$prefix.'/'.strtolower($module['dir']).'_', $prefix.'/'.strtolower($module['dir']).'/']))
-                        <ul class="nav flex-column ml-3">
-                        @foreach($Helper->getNavTop(Request::path()) as $dir => $name)
-                            @php $css = (Request::is($prefix.'/'.$dir) or strpos(Request::path(), $prefix.'/'.$dir.'/') !== false) ? ' active' : ''; @endphp
-                            <li class="nav-item">
-                                <a class="nav-link{{ $css }}" href="{{ asset($prefix.'/'.$dir) }}">─
-                                    @if(@++$cnt == 1){{  __('adminux.home') }} @else
-                                        @if(Lang::has('adminux.'.strtolower($name))){{ __('adminux.'.strtolower($name)) }}@else {{ $name }}@endif
-                                    @endif
-                                </a>
-                            </li>
-                        @endforeach
-                        </ul>
-                    @endif -->
-                </li>
-            @endforeach
-        </ul>
-        <h6 class="sidebar-heading px-3 mt-4 mb-1 text-muted">Configuration</h6>
-        <ul class="nav flex-column">
-            @foreach($menu as $module)
-                @if(!in_array($module['dir'], ['Admins', 'Partners', 'Services'])) @continue; @endif
-                <li class="nav-item">
-                    <a class="nav-link{{ (Request::is($prefix.'/'.strtolower($module['dir'])) or Illuminate\Support\Str::contains(Request::path(), [$prefix.'/'.strtolower($module['dir']), $prefix.'/'.strtolower($module['dir']).'/'])) ? ' active' : '' }}" href="{{ asset($prefix.'/'.strtolower($module['dir'])) }}">
-                        <span data-feather="{{ $module['icon'] }}"></span> {{ $module['name'] }}
+                    <a class="nav-link{{ (Request::is($prefix.'/'.$module['items'][0]['dir']) or Illuminate\Support\Str::contains(Request::path(), [$prefix.'/'.$module['items'][0]['dir'].'_', $prefix.'/'.$module['items'][0]['dir'].'/'])) ? ' active' : '' }}" href="{{ asset($prefix.'/'.$module['items'][0]['dir']) }}">
+                        <span data-feather="{{ $module['icon'] }}"></span> {{ $name }}
                     </a>
                 </li>
             @endforeach
         </ul>
+        @foreach($menu as $name => $module)
+            @if($name == 'default') @continue; @endif
+            <h6 class="sidebar-heading px-3 mt-4 mb-1 text-muted">@if(Lang::has('adminux.'.strtolower($name))){{ __('adminux.'.strtolower($name)) }}@else {{ $name }}@endif</h6>
+            @foreach($menu[$name] as $name => $module)
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link{{ (Request::is($prefix.'/'.$module['items'][0]['dir']) or Illuminate\Support\Str::contains(Request::path(), [$prefix.'/'.$module['items'][0]['dir'], $prefix.'/'.$module['items'][0]['dir'].'/'])) ? ' active' : '' }}" href="{{ asset($prefix.'/'.$module['items'][0]['dir']) }}">
+                            <span data-feather="{{ $module['icon'] }}"></span> {{ $name }}
+                        </a>
+                    </li>
+                </ul>
+            @endforeach
+        @endforeach
         <div class="position-absolute w-100 mb-1 px-3 fixed-bottom">
             <select class="custom-select custom-select-sm" id="partner_select">
                 <option value="">All Partners...</option>
