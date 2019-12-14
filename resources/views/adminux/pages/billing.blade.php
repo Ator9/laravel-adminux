3b9ed0e0572b74f7fc87b4c4ab85b6682e70d442
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-12-14 18:09:03 -0300

    billing

--- resources/views/adminux/pages/billing.blade.php
+++ resources/views/adminux/pages/billing.blade.php
@@ -1,7 +1,7 @@
 @extends('adminux.layout')
 
 @section('body')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 my-3 border-bottom">
     <h1 class="h2">Billing</h1>
     <div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group mr-2">
