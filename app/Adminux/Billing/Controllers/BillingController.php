54750129da7afcf665b595820ed04375eb429524
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-12-15 13:48:37 -0300

    billing

--- app/Adminux/Billing/Controllers/BillingController.php
+++ app/Adminux/Billing/Controllers/BillingController.php
@@ -8,19 +8,63 @@ class BillingController extends Controller
 {
     public function index()
     {
-        $res = \DB::table('accounts_products_usage')->where([
-            ['date_start', '>=', '2019-12-01'],
-            ['date_start', '<=', '2019-12-31 23:59:59'],
-        ])->orWhere([
-            ['date_start', '>=', '2019-12-01'],
-            ['date_start', '<=', '2019-12-31 23:59:59'],
-        ])
-        ->selectRaw('SUM(TIMESTAMPDIFF(HOUR, date_start, if(date_end, date_end, CURRENT_TIMESTAMP))) as hours')
-        ->selectRaw('product_id')->groupBy('product_id')
-        ->get()->keyBy('product_id')->toArray();
+        $usage = $this->getUsage([
+            'from' => '2019-11',
+            'to' => '2019-12',
+        ]);
 
-        dd($res);
+
+        foreach($usage as $date => $array) {
+            echo $date.'<hr>';
+            foreach($array as $pdi => $data) {
+                echo $pdi.': '.floor($data->minutes / 60).'<br>';
+            }
+        }
+
+        dd($usage);
 
         return view('adminux.pages.billing');
+    }
+
+    /**
+     * Get Products Usage (minutes)
+     *
+     * @return Collection All collections also serve as iterators (no "toArray()" needed). https://laravel.com/docs/eloquent-collections
+     */
+    public function getUsage($params = [])
+    {
+        $from = (!empty($params['from'])) ? $params['from'] : date('Y-m');
+        $to   = (!empty($params['to'])) ? $params['to'] : date('Y-m');
+
+        for($i = $from; $i <= $to; $i = date('Y-m', strtotime($i.' +1 month'))) {
+            $dates[] = $i;
+        }
+
+        foreach($dates as $date) {
+
+            list($year, $month) = explode('-', $date);
+
+            $hours[$date] = \DB::table('accounts_products_usage')->where([
+                ['date_start', '>=', $year.'-'.$month.'-01'],
+                ['date_start', '<', date('Y-m-01', mktime(0, 0, 0, $month+1, 1, $year))],
+            ])
+            ->orWhere(function ($query) use ($year, $month) {
+                $query->where('date_start', '<', $year.'-'.$month.'-01')->whereNull('date_end');
+            })
+            ->orWhere([
+                ['date_start', '<', $year.'-'.$month.'-01'],
+                ['date_end', '>=', $year.'-'.$month.'-01'],
+            ])
+            ->selectRaw('SUM(TIMESTAMPDIFF(MINUTE,
+                            if(date_start < "'.$year.'-'.$month.'-01", "'.$year.'-'.$month.'-01", date_start),
+                            if(
+                                if(date_end, date_end, CURRENT_TIMESTAMP) < "'.date('Y-m-01', mktime(0, 0, 0, $month+1, 1, $year)).'",
+                                if(date_end, date_end, CURRENT_TIMESTAMP), "'.date('Y-m-01', mktime(0, 0, 0, $month+1, 1, $year)).'")
+                        )) as minutes')
+            ->selectRaw('product_id')->groupBy('product_id')
+            ->get()->keyBy('product_id');
+        }
+
+        return $hours;
     }
 }
