53a82716db5030a7c469d915e7dbae01160c53ed
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-12-14 22:29:11 -0300

    billing

--- app/Adminux/Billing/Controllers/BillingController.php
+++ app/Adminux/Billing/Controllers/BillingController.php
@@ -6,13 +6,21 @@ use App\Http\Controllers\Controller;
 
 class BillingController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
+        $res = \DB::table('accounts_products_usage')->where([
+            ['date_start', '>=', '2019-12-01'],
+            ['date_start', '<=', '2019-12-31 23:59:59'],
+        ])->orWhere([
+            ['date_start', '>=', '2019-12-01'],
+            ['date_start', '<=', '2019-12-31 23:59:59'],
+        ])
+        ->selectRaw('SUM(TIMESTAMPDIFF(HOUR, date_start, if(date_end, date_end, CURRENT_TIMESTAMP))) as hours')
+        ->selectRaw('product_id')->groupBy('product_id')
+        ->get()->keyBy('product_id')->toArray();
+
+        dd($res);
+
         return view('adminux.pages.billing');
     }
 }
