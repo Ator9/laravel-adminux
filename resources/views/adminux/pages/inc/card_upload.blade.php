e0750921d8cdc7167d6151493c22560065c50f8e
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2019-12-21 08:00:14 -0300

    files

--- resources/views/adminux/pages/inc/card_upload.blade.php
+++ resources/views/adminux/pages/inc/card_upload.blade.php
@@ -16,7 +16,25 @@
             @csrf
         </form>
     </div>
-    <div class="card-body">
-        {{ print_r($files) }}
+    <div class="card-body py-0">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>URL</td>
+                    <td>Date</td>
+                </tr>
+            </thead>
+            @foreach($files as $value)
+                <tr>
+                    <td>{{ substr($value, strrpos($value, '/') + 1) }}</td>
+                    <td><a href="{{ url(str_replace('public', '', $value)) }}" target="_blank">{{ $value }}</a></td>
+                    <td>{{ $value }}</td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 </div>
+<style>
+.table thead td{border-top-width:0;border-bottom-width:0}
+</style>
