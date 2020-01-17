41768be91717c9b0a913cfb14f63ccd916a2ca6d
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2020-01-17 17:05:17 -0300

    billing

---
+++ app/Adminux/Billing/Models/BillingUnit.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Adminux\Billing\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BillingUnit extends Model
+{
+    protected $table = 'billing_units';
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [ 'product_id', 'units', 'date' ];
+}
