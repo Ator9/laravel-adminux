b62b599b3f8d9833715a1d7c3606d7e20ba2be71
Sebastian Gasparri <sgasparri@adverit.com>
2020-01-21 11:55:10 -0300

    pass

--- app/Adminux/ForgotPasswordController.php
+++ app/Adminux/ForgotPasswordController.php
@@ -9,28 +9,7 @@ use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 // Update .env APP_URL because it uses that to generate the reset password link
 class ForgotPasswordController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset emails and
-    | includes a trait which assists in sending these notifications from
-    | your application to your users. Feel free to explore this trait.
-    |
-    */
-
     use SendsPasswordResetEmails;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
 
     public function broker()
     {
