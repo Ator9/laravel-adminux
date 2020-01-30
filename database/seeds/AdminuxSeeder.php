9ff20a9a07c3429d0fee0e2ba427b79785102a30
Sebastian Gasparri <sgasparri@adverit.com>
2020-01-30 09:45:56 -0300

    seeder

---
+++ database/seeds/AdminuxSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+
+// php artisan db:seed --class=AdminuxSeeder
+class AdminuxSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        DB::table('accounts')->insert([
+            'partner_id' => 1,
+            'email' => $faker->unique()->safeEmail,
+            'password' => Hash::make('password'),
+            'account' => $faker->name,
+        ]);
+    }
+}
