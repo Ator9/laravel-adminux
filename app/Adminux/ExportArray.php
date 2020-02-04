c6074c53522e915140d4ae7ffecb0c7b1f9b6d41
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2020-02-04 18:54:05 -0300

    csv settings

--- app/Adminux/ExportArray.php
+++ app/Adminux/ExportArray.php
@@ -2,16 +2,23 @@
 
 namespace App\Adminux;
 use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
 
-class ExportArray implements FromArray
+class ExportArray implements FromArray, WithCustomCsvSettings
 {
-    public function __construct($array = [])
+    public function __construct($array = [], $settings = [])
     {
         $this->array = $array;
+        $this->settings = $settings;
     }
 
     public function array(): array
     {
         return $this->array;
+    }
+
+    public function getCsvSettings(): array
+    {
+        return !empty($this->settings) ? $this->settings : [];
     }
 }
