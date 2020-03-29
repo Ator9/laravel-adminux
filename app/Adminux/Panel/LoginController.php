05973ed119d88b7a2ebcf3107487e38cb3a063d3
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2020-03-29 13:47:46 -0300

    panel

--- app/Adminux/Panel/LoginController.php
+++ app/Adminux/Panel/LoginController.php
@@ -2,47 +2,12 @@
 
 namespace App\Adminux\Panel;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Adminux\Helper;
 
-class LoginController extends Controller
+class LoginController extends \App\Adminux\LoginController
 {
-    use AuthenticatesUsers;
-
-    protected $redirectTo = '/';
-
     protected function guard()
     {
         return auth('adminuxpanel');
-    }
-
-    public function showLoginForm()
-    {
-        if($this->guard()->check()) return redirect($this->redirectTo().'/'.config('adminux.base.default.login_redirect', 'dashboard'));
-        return view('adminux.login');
-    }
-
-    protected function loggedOut(Request $request)
-    {
-        return redirect($this->redirectTo());
-    }
-
-    function authenticated(Request $request, $user)
-    {
-        $user->last_login_ip = $request->getClientIp();
-        $user->last_login_at = \Carbon\Carbon::now();
-        $user->save();
-    }
-
-    // Check if active admin:
-    protected function credentials(Request $request)
-    {
-        return ['email' => $request->{$this->username()}, 'password' => $request->password, 'active' => 'Y'];
-    }
-
-    protected function redirectTo()
-    {
-        return request()->route()->getPrefix();
     }
 }
--- resources/views/adminux/login.blade.php
+++ resources/views/adminux/login.blade.php
@@ -16,7 +16,7 @@
                     <div class="card">
                         <div class="card-header">{{ config('adminux.base.default.project_name', 'Adminux') }}</div>
                         <div class="card-body">
-                            <form method="post" action="{{ route('login') }}">
+                            <form method="post" action="">
                                 @csrf
                                 <div class="form-group row">
                                     <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
