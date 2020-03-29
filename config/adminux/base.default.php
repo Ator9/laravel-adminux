cffb1b00b93c9b4e165811fe41af47cb2d0f2582
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2020-03-29 11:06:14 -0300

    routespanel

--- config/adminux/base.default.php
+++ config/adminux/base.default.php
@@ -5,7 +5,8 @@ if(file_exists(__DIR__.'/base.php')) return require(__DIR__.'/base.php');
 return [
 
     'project_name' => 'Adminux',
-    'custom_route_location' => app_path('Adminux/your_routes_path.php'),
+    'custom_routes' => app_path('Adminux/your_routes_path.php'),
+    'custom_routespanel' => app_path('Adminux/your_routespanel_path.php'),
     'login_redirect' => 'dashboard',
     'menu_software' => [], // list software name == menu name
 
