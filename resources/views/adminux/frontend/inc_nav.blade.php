74e03e5df55063342b2930606e891b24017b5e56
Sebastián Gasparri <2809743+Ator9@users.noreply.github.com>
2020-08-07 08:44:41 -0300

    bootstrap 4.5.2

--- resources/views/adminux/frontend/inc_nav.blade.php
+++ resources/views/adminux/frontend/inc_nav.blade.php
@@ -15,7 +15,7 @@
                     <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ __('adminux.products') }}</a>
                     <div class="dropdown-menu" aria-labelledby="dropdown01">
                         <a class="dropdown-item active" href="{{ url($prefix.'/products/2') }}">Cat</a>
-                        <a class="dropdown-item active" href="{{ url($prefix.'/products/2') }}">Cat</a>
+                        <a class="dropdown-item" href="{{ url($prefix.'/products/2') }}">Cat</a>
                     </div>
                 </li>
                 <li class="nav-item{% if nav == 'report'%} active{% endif %}">
